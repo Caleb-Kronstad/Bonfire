e76437bb3862c438c3858aae3f71a27f24890660
Caleb-Kronstad <calebtkronstad@gmail.com>
2025-10-07 01:35:20 -0400

    Interface style'

--- Engine/Source/Interface/Interface.cpp
+++ Engine/Source/Interface/Interface.cpp
@@ -22,6 +22,8 @@ namespace Bonfire
 		ImGuiIO& io = ImGui::GetIO();
 		io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
 		io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+
+		SetCustomStyle();
 		
 		Project& project = Project::GetInstance();
 		GLFWwindow* glfw_window = project.GetWindow().GetNativeWindow();
@@ -96,50 +98,60 @@ namespace Bonfire
 	void Interface::SetCustomStyle() // hard coded style for the time being, will update for customization later
 	{
 		ImGuiStyle* style = &ImGui::GetStyle();
+		ImVec4* colors = style->Colors;
+		
+		ImVec4 text_primary = ImVec4(1.00f, 1.00f, 1.00f, 1.00f);
+		ImVec4 bg_darkest = ImVec4(0.20f, 0.20f, 0.20f, 1.00f);
+		ImVec4 bg_dark = ImVec4(0.24f, 0.24f, 0.24f, 1.00f);
+		ImVec4 bg_main = ImVec4(0.28f, 0.28f, 0.28f, 1.00f);
+		ImVec4 bg_medium = ImVec4(0.33f, 0.33f, 0.33f, 1.00f);
+		ImVec4 bg_light = ImVec4(0.38f, 0.38f, 0.38f, 1.00f);
+		ImVec4 highlight = ImVec4(127.0f/255.0f, 84.0f/255.0f, 145.0f/255.0f, 1.00f);
+		ImVec4 highlight_bright = ImVec4(0.83f, 0.57f, 0.94f, 1.00f);
+		ImVec4 highlight_dark = ImVec4(0.549f, 0.259f, 0.675f, 1.0f);
 
-		style->WindowPadding = ImVec2(10, 15);
-		style->WindowRounding = 5.0f;
-		style->FramePadding = ImVec2(5, 0);
+		colors[ImGuiCol_Text] = text_primary;
+		colors[ImGuiCol_WindowBg] = bg_main;
+		colors[ImGuiCol_Border] = bg_darkest;
+		colors[ImGuiCol_FrameBg] = bg_darkest;
+		colors[ImGuiCol_FrameBgHovered] = highlight;
+		colors[ImGuiCol_FrameBgActive] = bg_medium;
+		colors[ImGuiCol_TitleBg] = bg_dark;
+		colors[ImGuiCol_TitleBgActive] = bg_medium;
+		colors[ImGuiCol_TitleBgCollapsed] = bg_darkest;
+		colors[ImGuiCol_SliderGrab] = highlight;
+		colors[ImGuiCol_SliderGrabActive] = highlight_bright;
+		colors[ImGuiCol_Header] = bg_medium;
+		colors[ImGuiCol_HeaderHovered] = highlight;
+		colors[ImGuiCol_HeaderActive] = highlight;
+		colors[ImGuiCol_TextSelectedBg] = highlight;
+		colors[ImGuiCol_CheckMark] = highlight;
+		colors[ImGuiCol_ButtonActive] = highlight;
+		colors[ImGuiCol_Separator] = highlight;
+		colors[ImGuiCol_SeparatorHovered] = highlight;
+		colors[ImGuiCol_SeparatorActive] = highlight;
+		colors[ImGuiCol_ResizeGrip] = highlight;
+		colors[ImGuiCol_ResizeGripHovered] = highlight;
+		colors[ImGuiCol_ResizeGripActive] = highlight;
+		colors[ImGuiCol_Tab] = bg_dark;
+		colors[ImGuiCol_TabHovered] = highlight;
+		colors[ImGuiCol_TabActive] = bg_main;
+		colors[ImGuiCol_TabUnfocused] = bg_darkest;
+		colors[ImGuiCol_TabUnfocusedActive] = bg_dark;
+		colors[ImGuiCol_DockingPreview] = highlight;
+		colors[ImGuiCol_DockingEmptyBg] = bg_darkest;
+
+		
+		style->WindowRounding = 4.0f;
 		style->FrameRounding = 4.0f;
-		style->ItemSpacing = ImVec2(12, 8);
-		style->ItemInnerSpacing = ImVec2(8, 6);
-		style->IndentSpacing = 25.0f;
-		style->ScrollbarSize = 15.0f;
-		style->ScrollbarRounding = 9.0f;
-		style->GrabMinSize = 5.0f;
 		style->GrabRounding = 3.0f;
-		style->ButtonTextAlign = ImVec2(0, 0);
-
-		style->Colors[ImGuiCol_Text] = ImVec4(0.9f, 0.9f, 0.9f, 1.0f);
-		style->Colors[ImGuiCol_TextDisabled] = ImVec4(0.4f, 0.4f, 0.4f, 1.0f);
-		style->Colors[ImGuiCol_FrameBg] = ImVec4(0.35f, 0.35f, 0.35f, 1.0f);
-		style->Colors[ImGuiCol_FrameBgActive] = ImVec4(0.4f, 0.4f, 0.4f, 1.0f);
-		style->Colors[ImGuiCol_FrameBgHovered] = ImVec4(0.55f, 0.55f, 0.55f, 1.0f);
-		style->Colors[ImGuiCol_Button] = ImVec4(0.35f, 0.35f, 0.35f, 1.0f);
-		style->Colors[ImGuiCol_ButtonActive] = ImVec4(0.4f, 0.4f, 0.4f, 1.0f);
-		style->Colors[ImGuiCol_ButtonHovered] = ImVec4(0.55f, 0.55f, 0.55f, 1.0f);
-		style->Colors[ImGuiCol_SliderGrab] = ImVec4(0.7f, 0.7f, 0.7f, 1.0f);
-		style->Colors[ImGuiCol_SliderGrabActive] = ImVec4(0.6f, 0.6f, 0.6f, 1.0f);
-		style->Colors[ImGuiCol_Border] = ImVec4(0.0f, 0.0f, 0.0f, 0.0f);
-		style->Colors[ImGuiCol_CheckMark] = ImVec4(0.9f, 0.9f, 0.9f, 1.0f);
-		style->Colors[ImGuiCol_TitleBg] = ImVec4(0.35f, 0.35f, 0.35f, 1.0f);
-		style->Colors[ImGuiCol_TitleBgActive] = ImVec4(0.45f, 0.45f, 0.45f, 1.0f);
-		style->Colors[ImGuiCol_TitleBgCollapsed] = ImVec4(0.2f, 0.2f, 0.2f, 1.0f);
-		style->Colors[ImGuiCol_ResizeGrip] = ImVec4(0.2f, 0.2f, 0.2f, 1.0f);
-		style->Colors[ImGuiCol_ResizeGripActive] = ImVec4(0.4f, 0.4f, 0.4f, 1.0f);
-		style->Colors[ImGuiCol_ResizeGripHovered] = ImVec4(0.3f, 0.3f, 0.3f, 1.0f);
-		style->Colors[ImGuiCol_WindowBg] = ImVec4(0.25f, 0.25f, 0.25f, 1.0f);
-		style->Colors[ImGuiCol_ChildBg] = ImVec4(0.25f, 0.25f, 0.25f, 1.0f);
-		style->Colors[ImGuiCol_Header] = ImVec4(0.35f, 0.35f, 0.35f, 1.0f);
-		style->Colors[ImGuiCol_HeaderActive] = ImVec4(0.4f, 0.4f, 0.4f, 1.0f);
-		style->Colors[ImGuiCol_HeaderHovered] = ImVec4(0.45f, 0.45f, 0.45f, 1.0f);
-
-		style->WindowRounding = 0.0f;
-		style->ChildRounding = 0.0f;
-		style->FrameRounding = 0.0f;
-		style->GrabRounding = 0.0f;
-		style->PopupRounding = 0.0f;
-		style->ScrollbarRounding = 0.0f;
+		style->PopupRounding = 4.0f;
+		style->TabRounding = 4.0f;
+		style->WindowMenuButtonPosition = ImGuiDir_Right;
+		style->ScrollbarSize = 10.0f;
+		style->GrabMinSize = 10.0f;
+		style->DockingSeparatorSize = 1.0f;
+		style->SeparatorTextBorderSize = 2.0f;
 	}
 
 	void Interface::SetClearStyle()
